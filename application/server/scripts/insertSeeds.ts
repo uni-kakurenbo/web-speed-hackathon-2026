4b37b6f479fbca2ceaa9e73a0e4cff28cd085cc0
uni-kakurenbo <kohma1116@gmail.com>
2026-03-20 15:35:32 +0900

    mariaDB

--- application/server/scripts/insertSeeds.ts
+++ application/server/scripts/insertSeeds.ts
@@ -1,17 +1,19 @@
-import * as fs from "node:fs/promises";
-
 import { Sequelize } from "sequelize";
 
 import { initModels } from "@web-speed-hackathon-2026/server/src/models";
-import { DATABASE_PATH } from "@web-speed-hackathon-2026/server/src/paths";
 import { insertSeeds } from "@web-speed-hackathon-2026/server/src/seeds";
 
-await fs.rm(DATABASE_PATH, { force: true, recursive: true });
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  logging: false,
-  storage: DATABASE_PATH,
-});
+const sequelize = new Sequelize(
+  process.env["NS_MARIADB_DATABASE"] || "web_speed_hackathon",
+  process.env["NS_MARIADB_USER"] || "root",
+  process.env["NS_MARIADB_PASSWORD"] || "",
+  {
+    host: process.env["NS_MARIADB_HOSTNAME"] || "127.0.0.1",
+    port: Number(process.env["NS_MARIADB_PORT"]) || 3306,
+    dialect: "mariadb",
+    logging: false,
+  }
+);
 initModels(sequelize);
 await sequelize.sync({ force: true, logging: false });
 await insertSeeds(sequelize);
--- application/server/src/paths.ts
+++ application/server/src/paths.ts
@@ -5,4 +5,3 @@ const __dirname = import.meta.dirname;
 export const PUBLIC_PATH = path.resolve(__dirname, "../../public");
 export const UPLOAD_PATH = path.resolve(__dirname, "../../upload");
 export const CLIENT_DIST_PATH = path.resolve(__dirname, "../../dist");
-export const DATABASE_PATH = path.resolve(__dirname, "../database.sqlite");
